330ffc5fbc6213704712911b0df2dc905b672dbf
OksanaNova <o.nova0804@gmail.com>
2025-07-01 17:17:37 -0600

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
+const routes = require('./MealRoutes');
 
 require('dotenv').config();
 
-// mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", false);
 
 
 const PORT = 4000 || process.env.port
@@ -14,6 +14,8 @@ mongoose
 .connect(process.env.MONGODB_LINK)
 .then(() => console.log('WE WERE CONNECTED TO MONGO'))
 .catch((err) => console.log(err))
+
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log(`I AM LISTENING ON PORT ${PORT}`)
