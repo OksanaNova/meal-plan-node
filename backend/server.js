bb587184ffd5e4d5a7ac413d575fe1ea242ff03e
OksanaNova <o.nova0804@gmail.com>
2025-07-01 16:39:19 -0600

    connection mongo

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,23 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
 
-const PORT = 4000;
+const PORT = 4000 || process.env.port
+
+mongoose
+.connect(process.env.MONGODB_LINK)
+.then(() => console.log('WE WERE CONNECTED TO MONGO'))
+.catch((err) => console.log(err))
 
 app.listen(PORT, () => {
     console.log(`I AM LISTENING ON PORT ${PORT}`)
 })
 
-// oksana VrIEzPYhTK9ti2DA VrIEzPYhTK9ti2DA
+// oksana VrIEzPYhTK9ti2DA 
+
+// mongodb+srv://oksana:<db_password>@cluster0.fyb72nt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
