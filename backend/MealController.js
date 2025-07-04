00505deaf2d290f54280fa155fd72eaa170150c0
OksanaNova <o.nova0804@gmail.com>
2025-07-03 20:03:25 -0600

    controller added

--- backend/MealController.js
+++ backend/MealController.js
@@ -1,1 +1,16 @@
 const MealModel = require('./MealModel');
+
+// GET
+
+module.exports.getMeal = async (req, res) => {
+    const myMeal = await MealModel.find();
+    res.send(myMeal)
+}
+
+module.exports.saveMeals = async (req, res) => { 
+    const { title } = req.body;
+    MealModel.create({ title })
+    .then((data) => { console.log('Meal added')
+        res.send(data)
+    })
+}
